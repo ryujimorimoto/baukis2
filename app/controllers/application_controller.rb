acbd718681b1ac3a6c0a0000d5a00648de15adb2
ryujimorimoto <forest.book.programmer@gmail.com>
2020-09-23 20:49:28 +0900

    docker-composer.ymlを修正し、コンテナにログイン可能にした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+rescue_from StandardError, with: :rescue500
+
   private
   def set_layout
-    if params[:controller].match(%r{\A(staff|admin|customer)/})
+    # NOTE:  params[:controller]から、controller_pathに変更。専用メソッドを使って切り分けるべきであり、paramsは使わない方が良いと考える。
+    if controller_path.match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "customer"
@@ -12,5 +15,8 @@ class ApplicationController < ActionController::Base
   def rescue403(e)
     @exception = e
     render template: "errors/forbidden", status: 403
+  end
+  def rescue500(e)
+    render template: "errors/internal_server_error", status: 500
   end
 end
